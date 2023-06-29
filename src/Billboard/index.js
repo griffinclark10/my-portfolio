39ffffbe643a4104b273af532b577011a44812a3
Griffin Clark <griffinclark10@gmail.com>
2023-06-29 12:47:17 +0800

    button main page

--- src/Billboard/index.js
+++ src/Billboard/index.js
@@ -1,3 +1,7 @@
+import LinkButton from '../Buttons/Links'
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
 import './index.scss'
 
 const Billboard = () => {
@@ -6,10 +10,12 @@ const Billboard = () => {
             <div className="black-box"></div>
             <div className="bottom">
                 <div className='column'>
-                    <a class="btn" href="#"><i class="ion-ios-arrow-down"></i></a>
-                    <a class="btn" href="#"><i class="ion-ios-arrow-down"></i></a>
-                    <a class="btn" href="#"><i class="ion-ios-arrow-down"></i></a>
-                    <a class="btn" href="#"><i class="ion-ios-arrow-down"></i></a>
+                    <ul>
+                        <li><LinkButton href='#' icon={ faHouse }/></li>
+                        <li><LinkButton href='#' icon={ faGithub }/></li>
+                        <li><LinkButton href='#' icon={ faLinkedin }/></li>
+                        <li><LinkButton href='#' icon={ faFilePdf }/></li>
+                    </ul>
                 </div>
                 <div className='column'>
                     <div className='label-visual'>
--- src/Buttons/Links/index.js
+++ src/Buttons/Links/index.js
@@ -1,9 +1,12 @@
-import 'index.scss'
+import './index.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const LinkButton = (props) => {
-    return (
-        <h1>Hello</h1>
-    )
-}
+  return (
+    <a className="btn" href={props.href}>
+      <FontAwesomeIcon icon={props.icon} className='icon'/>
+    </a>
+  );
+};
 
-export default LinkButton
+export default LinkButton;
