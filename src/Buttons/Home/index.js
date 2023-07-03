094198e57e2f42c48f12dbcfffd57eb482e07926
Griffin Clark <griffinclark10@gmail.com>
2023-07-03 14:10:01 +0800

    home buttons

--- src/Buttons/Home/index.js
+++ src/Buttons/Home/index.js
@@ -1,10 +1,11 @@
+import './index.scss'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const HomeButton = ({ icon, text }) => {
     return (
         <button className="home-button">
             <p className="btn-text">{ text }</p>
-            <FontAwesomeIcon icon={icon} className={icon} />
+            <FontAwesomeIcon icon={icon} className="icon" />
         </button>
     );
 };
--- src/Home/index.js
+++ src/Home/index.js
@@ -1,11 +1,19 @@
-import Billboard from '../Billboard'
 import './index.scss'
+import HomeButton from '../Buttons/Home'
+import { faBriefcase, faProjectDiagram, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Home = () => {
     return (
         <>
-            <div className='griffin'>__GRIFFIN_CLARK'S__</div>
-            <div className='portfolio'>_PORTFOLIO_</div>
+            <div className='col-top'>
+                <div className='griffin'>_____GRIFFIN_CLARK'S______</div>
+                <div className='portfolio'>____PORTFOLIO____</div>
+            </div>
+            <div className='col-bottom'>
+                <HomeButton text="About Me" icon={faUser} />
+                <HomeButton text="Experience" icon={faBriefcase} />
+                <HomeButton text="Projects" icon={faProjectDiagram} />
+            </div>
         </>          
         
     )
