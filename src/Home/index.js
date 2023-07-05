f27a6a78ce4fadf404481b6fa7b0f0eeab99ebf2
Griffin Clark <griffinclark10@gmail.com>
2023-07-05 12:26:00 +0800

    homepage doneish

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,18 +1,18 @@
 import './index.scss'
 import HomeButton from '../Buttons/Home'
-import { faBriefcase, faProjectDiagram, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faBriefcase, faLaptopCode, faPersonRays } from '@fortawesome/free-solid-svg-icons'
 
 const Home = () => {
     return (
         <>
             <div className='col-top'>
-                <div className='griffin'>_____GRIFFIN_CLARK'S______</div>
+                <div className='griffin'>______GRIFFIN_CLARK'S_______</div>
                 <div className='portfolio'>____PORTFOLIO____</div>
             </div>
             <div className='col-bottom'>
-                <HomeButton text="About Me" icon={faUser} />
+                <HomeButton text="About Me" icon={faPersonRays} />
                 <HomeButton text="Experience" icon={faBriefcase} />
-                <HomeButton text="Projects" icon={faProjectDiagram} />
+                <HomeButton text="Projects" icon={faLaptopCode} />
             </div>
         </>          
         
--- src/HoverTextContainer/index.js
+++ src/HoverTextContainer/index.js
@@ -4,10 +4,10 @@ const HoverTextContainer = ({ showText }) => {
     return (
         <div className="text-container">
             {/* {showText === null && <p>null</p>} */}
-            {showText === 1 && <p>Take Me Home</p>}
-            {showText === 2 && <p>My Github</p>}
-            {showText === 3 && <p>My Linkedin</p>}
-            {showText === 4 && <p>My Resume</p>}
+            {showText === 1 && <p>TAKE ME HOME</p>}
+            {showText === 2 && <p>MY GITHUB</p>}
+            {showText === 3 && <p>MY LINKEDIN</p>}
+            {showText === 4 && <p>MY RESUME</p>}
         </div>
     );
 };
