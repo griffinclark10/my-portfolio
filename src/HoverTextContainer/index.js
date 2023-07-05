67ee81a8e79961b49f20e6b1d29344e24998612e
Griffin Clark <griffinclark10@gmail.com>
2023-07-05 12:52:46 +0800

    more homepage stuff

--- src/HoverTextContainer/index.js
+++ src/HoverTextContainer/index.js
@@ -4,7 +4,7 @@ const HoverTextContainer = ({ showText }) => {
     return (
         <div className="text-container">
             {/* {showText === null && <p>null</p>} */}
-            {showText === 1 && <p>TAKE ME HOME</p>}
+            {showText === 1 && <p>BACK HOME</p>}
             {showText === 2 && <p>MY GITHUB</p>}
             {showText === 3 && <p>MY LINKEDIN</p>}
             {showText === 4 && <p>MY RESUME</p>}
