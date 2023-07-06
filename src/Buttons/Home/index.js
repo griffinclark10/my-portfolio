08f81e44712eae0edca72b93a07a3af238715c44
Griffin Clark <griffinclark10@gmail.com>
2023-07-06 18:09:57 +0800

    added react framer to homepage buttons

--- src/Buttons/Home/index.js
+++ src/Buttons/Home/index.js
@@ -1,15 +1,20 @@
 import './index.scss'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
 
 const HomeButton = ({ icon, text, href }) => {
     return (
-        <Link to={href} >
-            <button className="home-button">
-                <p className="btn-text">{ text }</p>
-                <FontAwesomeIcon icon={icon} className="icon" />
-            </button>
-        </Link>
+        <motion.div
+            whileHover={{ scale: 1.2 }} whileTap={{ scale: 1.0 }}>
+            <Link to={href} >
+                <button className="home-button">
+                    <p className="btn-text">{ text }</p>
+                    <FontAwesomeIcon icon={icon} className="icon" />
+                </button>
+            </Link>
+        </motion.div>
     );
 };
 
